78fc622f7bf4350631d97491bd407c742592d454
Navjot <navjot.654404@gmail.com>
2026-05-02 20:27:36 +0530

    updated loops code

--- loops.py
+++ loops.py
@@ -1,3 +1,26 @@
-a=5
-if a>4:
-    print("right")
+# LOOP
+
+nums=[2,4,6,8]
+for i in nums:
+    print(i)
+# task
+# Q1
+nums=[1,3,5]
+for i in nums:
+    print(i*2)
+# Q2
+nums=[2,4,6,8]
+for i in nums:
+        if i%2==0:
+             print(i)
+# Q3
+nums=[3,6,9,12]
+for i in nums:
+     if i%3==0:
+          print(i)
+# Q4
+nums=[1,2,3,4,5,6]
+for i in nums:
+     if i%2==0:
+          print(i*2)
+          
